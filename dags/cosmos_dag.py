803af9cf134d3378d3aacf7eb24b54e0fde3d19a
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-08-13 10:36:03 +0200

    Attempt to fix https://github.com/astronomer/astronomer-cosmos/issues/1874
    Without further digging into the dbt project, I attempted to isolate the views execution, assuming that was causing the error:
    
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO - 08:31:56  Failure in model allergyintolerance_reference_encounter_unresolved (models/metrics/data_quality/allergyintolerance_reference_encounter_unresolved.sql)
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO - 08:31:56    Compilation Error in model allergyintolerance_reference_encounter_unresolved (models/metrics/data_quality/allergyintolerance_reference_encounter_unresolved.sql)
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO -   'NoneType' object is not iterable
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO -
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO -   > in macro _are_inputs_available (macros/metric_processing_macros/calculate_metric.sql)
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO -   > called by macro calculate_metric (macros/metric_processing_macros/calculate_metric.sql)
    [2025-08-13, 08:31:56 UTC] {subprocess.py:90} INFO -   > called by model allergyintolerance_reference_encounter_unresolved (models/metrics/data_quality/allergyintolerance_reference_encounter_unresolved.sql)
    
    Unfortunately, this did not work.
    
    We'll need to fully understand the dbt project and come up with a better solution.

--- dags/cosmos_dag.py
+++ dags/cosmos_dag.py
@@ -1,8 +1,13 @@
 from datetime import datetime
+import os
+from datetime import datetime
+from pathlib import Path
 
-from cosmos.airflow.dag import DbtDag
-from cosmos.config import ProjectConfig, RenderConfig
-from cosmos.constants import LoadMode, InvocationMode, TestBehavior
+from airflow import DAG
+
+from cosmos import DbtDag, DbtTaskGroup
+from cosmos.config import ExecutionConfig, ProjectConfig, RenderConfig
+from cosmos.constants import ExecutionMode, LoadMode, InvocationMode, TestBehavior
 from include.profiles import bigquery_db
 from include.constants import venv_execution_config
 
@@ -13,6 +18,9 @@ cosmos_dag = DbtDag(
     project_config=ProjectConfig(Path("/usr/local/airflow/dbt/fhir-dbt-analytics")),
     profile_config=bigquery_db,
     execution_config=venv_execution_config,
+    render_config=RenderConfig(
+        test_behavior=TestBehavior.NONE
+    ),
     # normal dag parameters
     schedule=None,
     start_date=datetime(2023, 1, 1),
@@ -23,3 +31,48 @@ cosmos_dag = DbtDag(
         "retries": 2,
     },
 )
+
+
+with DAG(
+    dag_id="cosmos_task_group",
+    schedule=None,
+    catchup=False,
+    start_date=datetime(2025, 8, 1),
+    default_args={"retries": 0},
+):
+
+    views = DbtTaskGroup(
+        group_id="views",
+        project_config=ProjectConfig(Path("/usr/local/airflow/dbt/fhir-dbt-analytics")),
+        render_config=RenderConfig(
+            select=["+config.materialized:view"],
+            exclude=["*metric*"],
+            enable_mock_profile=False,
+            test_behavior=TestBehavior.NONE
+        ),
+        execution_config=venv_execution_config,
+        operator_args={"install_deps": True},
+        profile_config=bigquery_db,
+    )
+
+    non_views = DbtTaskGroup(
+        group_id="views_downstream",
+        project_config=ProjectConfig(Path("/usr/local/airflow/dbt/fhir-dbt-analytics")),
+        render_config=RenderConfig(
+            exclude=["config.materialized:view"],
+            select=["config.materialized:view+", "*metric*"],
+            enable_mock_profile=False,
+            test_behavior=TestBehavior.NONE
+        ),
+        execution_config=ExecutionConfig(
+            execution_mode=ExecutionMode.AIRFLOW_ASYNC,
+            async_py_requirements=[f"dbt-bigquery"],
+        ),
+        operator_args={
+            "location": "US",
+            "install_deps": True
+        },
+        profile_config=bigquery_db,
+    )
+
+    views >> non_views
