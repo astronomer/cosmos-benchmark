6b322a8ca358954090fbe8bc80a570607de88c9e
pankajastro <pankaj.singh@astronomer.io>
2025-08-31 03:16:46 +0530

    Add dbt project for async dag

--- include/profiles.py
+++ include/profiles.py
@@ -9,7 +9,7 @@ bigquery_db = ProfileConfig(
     profile_mapping=GoogleCloudServiceAccountDictProfileMapping(
         conn_id="bigquery_conn",
         # Dataset for Airflow 2 Deployment
-        profile_args={"project": "astronomer-dag-authoring", "dataset": "fhir_airflow2"},
+        profile_args={"project": "astronomer-airflow-providers", "dataset": "test_async"},
         # Dataset for Airflow 3 Deployment
         # profile_args={"project": "astronomer-dag-authoring", "dataset": "fhir_airflow3"},
     )
