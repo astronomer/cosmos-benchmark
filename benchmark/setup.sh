3ba778ec3d6878c8026c24ce967b2d5b095ee2fb
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-07 15:25:48 +0100

    Benchmark single dbt command versus multiple (#5)
    Add benchmark comparison between running a dbt Core pipeline with a
    single command, and running the same pipeline using multiple commands.
        
    This is a follow-up from: #4
    
    An example of run:
    ```
    Metrics for Pod: dbt-core-run-per-model-5rfzh (last 240h)
    ----------------------------------------------
    Max CPU Utilization (cores):    0.4504490431333661
    Stddev CPU Utilization (cores): 0.08225790503612171
    Max Memory Usage:     432 MiB
    Stddev Memory Usage:  59 MiB
    Handling connection for 9090
    Handling connection for 9090
    Job Duration: 00:31:04
    job.batch "dbt-core-run-per-model" deleted
    
    ```

--- benchmark/setup.sh
+++ benchmark/setup.sh
@@ -17,9 +17,15 @@ helm install prometheus prometheus-community/kube-prometheus-stack \
   --namespace monitoring --create-namespace \
   --set prometheus.prometheusSpec.scrapeInterval="5s"
 
+# Wait for Prometheus pod to be ready
+kubectl wait --for=condition=ready pod \
+  -l app.kubernetes.io/name=prometheus \
+  -n monitoring --timeout=180s
+
 # Expose Prometheus to the host so we can fetch metrics
 kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090 &
 
 # Build the docker image that will be used to run the experiments
-cd ..; docker build -t benchmark:0.0.1 -f benchmark/Dockerfile .
-kind load docker-image benchmark:0.0.1
+cd ..; docker build -t benchmark:0.0.2 -f benchmark/Dockerfile .
+kind load docker-image benchmark:0.0.2
+
