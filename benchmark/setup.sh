8b23266c2f87b5d76b6678268fd336418b4d1a62
Tatiana Al-Chueyr <tatiana.alchueyr@gmail.com>
2025-10-07 09:08:41 -0700

    Clean up

--- benchmark/setup.sh
+++ benchmark/setup.sh
@@ -33,7 +33,6 @@ kubectl create namespace airflow
 ## Install Airflow using Helm
 helm install airflow apache-airflow/airflow --version 1.16.0 \
   --namespace airflow \
-  -f values.yaml
+  -f pre-process/values.yaml
 
-kubectl port-forward svc/prometheus-kube-prometheus-prometheus -n monitoring 9090 &
 kubectl port-forward svc/airflow-webserver 8080:8080 -n airflow &
